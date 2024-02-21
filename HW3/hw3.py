9430f6040b785ab54c905ee2ac5877c50245f7c4
jung1230 <alanchen20072@gmail.com>
2024-02-21 16:06:15 -0500

    finish HW3

--- HW3/hw3.py
+++ HW3/hw3.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 from PIL import Image
+import cvxpy as cvx 
 
 
 
@@ -16,7 +17,6 @@ Y = plt.imread('cat_grass.jpg') / 255
 # -------------------- (b: train by getting parameters) --------------------
 row1, col1 = np.shape(train_cat)
 u1 = np.zeros(row1)
-print(row1, col1)
 # Loop through each row of the array and calculate the mean of the current row( SHOULD I CHANGE THIS TO COL?????????)
 for i in range(row1):
     u1[i] = np.mean(train_cat[i,:])
@@ -117,27 +117,116 @@ image.save('output4Panda.png')
 
 # -------------------- (b) --------------------
 true_positive = 0 # true for actual, Positive for predicted
-total_positive4truth = 0
+total_positive4truth = np.count_nonzero(truth)
 false_positive = 0 # false for actual, positive for predicted
-total_negative4truth = 0
+total_negative4truth = len(truth.ravel()) - total_positive4truth # ravel: Return a contiguous flattened array.
 
-prediction4Cat = plt.imread('output4cat.png')# skip step 2 and run this code can be faster
-rowp , colp = prediction4Cat.shape
+# left part of comparison 
+d = 64 # train_cat.shape = (64, 1976)
+abs_Sigma1 = np.linalg.det(sigma1)
+abs_Sigma0 = np.linalg.det(sigma0)
+inv_Sigma1 = np.linalg.inv(sigma1)
+inv_Sigma0 = np.linalg.inv(sigma0)
 
-truth = plt.imread('truth.png') 
-
-
-
+div = np.zeros((rowp,colp))
 for i in range(rowp - 8):
     for j in range(colp - 8):
-        if(truth[i, j] == 1): # if it is white(cat)
-            total_positive4truth += 1
-        else:
-            total_negative4truth += 1
+        block = Y[i: i+8, j: j+8]
+        block = np.reshape(block,(64,1))
+        PofXgivenC1 = 1/(((2*np.pi)**(d/2)) * ((abs_Sigma1)**(0.5))) * np.exp(-0.5 * (block - u1).T @ inv_Sigma1 @ (block - u1))
+        PofXgivenC0 = 1/(((2*np.pi)**(d/2)) * ((abs_Sigma0)**(0.5))) * np.exp(-0.5 * (block - u0).T @ inv_Sigma0 @ (block - u0))
+        div[i,j] = np.log(PofXgivenC1[0,0] / PofXgivenC0[0,0]) # !!!! try:change to np.sum and mean!!!!
+
+n_tau = 50
+PF = np.zeros(n_tau)
+PD = np.zeros(n_tau)
+tauset = np.linspace(-200,200,n_tau)
+for k in range(n_tau):
+    tau = (tauset[k])
+    prediction = np.zeros(Y.shape)
+    true_positive = 0; 
+    false_positive = 0; 
+    prediction = div > tau
+    for i in range(rowp - 8):
+        for j in range(colp - 8):
+            if (prediction[i][j]==1) and (truth[i][j]>=0.5): true_positive  += 1
+            if (prediction[i][j]==1) and (truth[i][j]<=0.5): false_positive += 1
+
+
+    PD[k] = true_positive / total_positive4truth
+    PF[k] = false_positive / total_negative4truth
+
+plt.figure()
+plt.plot(PF,PD)
+plt.xlabel('PF')
+plt.ylabel('PD')
+plt.title('ROC curve')
+plt.xlim(0, 1)  
+plt.ylim(0, 1) 
+# plt.show() 
+
         
-        if(truth[i, j] == 1 and prediction_from_pic[i, j] == 1):
-            true_positive += 1
-        if(truth[i, j] == 0 and prediction_from_pic[i, j] == 1):
-            false_positive += 1
-        
-    
+# -------------------- (c) --------------------
+tau = 4.836
+prediction = np.zeros(Y.shape)
+true_positive = 0; 
+false_positive = 0; 
+prediction = div > tau
+for i in range(rowp - 8):
+    for j in range(colp - 8):
+        if (prediction[i][j]==1) and (truth[i][j]>=0.5): true_positive  += 1
+        if (prediction[i][j]==1) and (truth[i][j]<=0.5): false_positive += 1
+
+
+plt.scatter(false_positive / total_negative4truth, true_positive / total_positive4truth, s=100, c='red', label='Specific Point')  
+plt.show() 
+
+# -------------------- (d) --------------------
+X1 = train_cat.T
+X0 = train_grass.T
+A = np.vstack((X1, X0))
+Ones4X1 = np.ones((col1,1))
+Ones4X0 = np.ones((col0,1)) 
+b = np.vstack((Ones4X1,-1 * Ones4X0))
+
+theta = cvx.Variable((d,1))
+objective = cvx.Minimize(cvx.sum_squares(A @ theta - b)) # sum_squares for 2-norm, norm for 1-norm
+prob = cvx.Problem(objective)
+prob.solve()
+theta_hat = theta.value
+
+
+thetaX = np.zeros((rowp,colp))
+for i in range(rowp - 8):
+    for j in range(colp - 8):
+        block = Y[i: i+8, j: j+8]
+        block = np.reshape(block,(64,1))
+        thetaX[i,j] = theta_hat.T @ block
+
+n_tau = 50
+PF = np.zeros(n_tau)
+PD = np.zeros(n_tau)
+tauset = np.linspace(-1.2, -0.1, n_tau)
+for k in range(n_tau):
+    tau = (tauset[k])
+    prediction = np.zeros(Y.shape)
+    true_positive = 0; 
+    false_positive = 0; 
+    prediction = thetaX > tau
+    for i in range(rowp - 8):
+        for j in range(colp - 8):
+            if (prediction[i][j]==1) and (truth[i][j]>=0.5): true_positive  += 1
+            if (prediction[i][j]==1) and (truth[i][j]<=0.5): false_positive += 1
+
+
+    PD[k] = true_positive / total_positive4truth
+    PF[k] = false_positive / total_negative4truth
+
+plt.figure()
+plt.plot(PF,PD)
+plt.xlabel('PF')
+plt.ylabel('PD')
+plt.title('ROC curve')
+plt.xlim(0, 1)  
+plt.ylim(0, 1) 
+plt.show() 
